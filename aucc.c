7dfd43219ca1f1a71054e63dddb1a096f7d54a6e
Linus Aurora <LinusAUR@outlook.com>
2024-11-19 18:01:40 +0800

    Adds addition and subtraction.

--- aucc.c
+++ aucc.c
@@ -4,16 +4,36 @@
 int main (int argc, char** argv) {
     if (argc != 2) {
         fprintf(stderr, "Invalid argument number");
+        return 1;
     }
+
+    char* p = argv[1];
     
     puts(".intel_syntax noprefix");
-    
     puts(".globl main");
     puts("main:");
-    printf("    MOV RAX, %d\n", atoi(argv[1]));
+
+
+    printf("    MOV RAX, %ld\n", strtol(p, &p, 10));
+
+    while(*p) {
+        if (*p == '+') {
+            p++;
+            printf("    ADD RAX, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        if (*p == '-') {
+            p++;
+            printf("    SUB RAX, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "Unexpected character: '%c'", *p);
+    }
     printf("    RET\n");
 
-    
+
     puts(".section .note.GNU-stack,\"\",@progbits");
     
     return 0;
